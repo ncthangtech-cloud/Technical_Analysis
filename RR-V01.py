c635c18092ea8b8e3a212126afd5712460699be9
ncthangtech-cloud <ncthang.tech@gmail.com>
2025-09-14 11:49:30 +0700

    Update RR-V01.py

--- RR-V01.py
+++ RR-V01.py
@@ -67,7 +67,7 @@ if not st.session_state.authenticated:
                 st.session_state.authenticated = True
                 st.session_state.username = username
                 st.success(f"Welcome, {username}!")
-                st.experimental_rerun()  # refresh UI
+                st.rerun()  # refresh UI
             else:
                 st.error("Invalid username or password")
 
